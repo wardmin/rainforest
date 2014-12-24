345b761eff62ec9e15be1dada5ba0357e8e7ccc7
Ward Minnis <ward@minnis.net>
2014-12-24 16:24:49 -0500

    linked the application controller flash message to bootstrap styling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,7 @@ class ApplicationController < ActionController::Base
 
   def ensure_logged_in
     unless current_user
-      flash[:alert] = "Please log in"
+      flash[:warning] = "Please log in."
       redirect_to new_session_path
     end
   end
