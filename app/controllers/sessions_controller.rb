8cff608ddbb970ddd582c36107f14bda536abbb6
Ward Minnis <ward@minnis.net>
2014-12-24 15:56:11 -0500

    flash message working with bootstrap styles

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,16 +6,16 @@ class SessionsController < ApplicationController
   	user = User.find_by_email(params[:email])
   	if user && user.authenticate(params[:password])
   		session[:user_id] = user.id
-      flash[:notice] = "Logged in!"
+      flash[:success] = "Logged in!"
   		redirect_to products_url
   	else
-  		flash.now[:alert] = "Invalid email or password"
+  		flash.now[:warning] = "Invalid email or password"
   		render "new"
   	end
   end
 
   def destroy
   	session[:user_id] = nil
-  	redirect_to products_url, notice: "Logged out!"
+  	redirect_to products_url, success: "Logged out!"
   end
 end
