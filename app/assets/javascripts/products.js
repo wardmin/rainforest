0fb0c6363e247f03441c3497efa750ac09c433de
Ward Minnis <ward@minnis.net>
2015-01-14 16:11:03 -0500

    fixed some pagination issues and disabled infinite scrolling

--- app/assets/javascripts/products.js
+++ app/assets/javascripts/products.js
@@ -1,21 +1,21 @@
-$(document).on('ready page:load', function() {
-	$('#search-form').submit(function(event) {
-		event.preventDefault();
-		var searchValue = $('#search').val();
+// $(document).on('ready page:load', function() {
+// 	$('#search-form').submit(function(event) {
+// 		event.preventDefault();
+// 		var searchValue = $('#search').val();
 
-		$.getScript('/products?search=' + searchValue);
-	});
-});
+// 		$.getScript('/products?search=' + searchValue);
+// 	});
+// });
 
-$(document).on('ready page:load', function() {
-	$(window).scroll(function() {
-		if ( $(window).scrollTop() > ($(document).height() - $(window).height() - 50)) {
-			var nextPage = $('.pagination .next a');
-			if ( nextPage.length > 0 ) {
-				var href = nextPage.attr('href');
-				$.getScript(href);
-			}	
-		}
-	});
-	$('.alert').delay(5000).fadeOut(1000);
-})
+// $(document).on('ready page:load', function() {
+// 	$(window).scroll(function() {
+// 		if ( $(window).scrollTop() > ($(document).height() - $(window).height() - 50)) {
+// 			var nextPage = $('.pagination .next a');
+// 			if ( nextPage.length > 0 ) {
+// 				var href = nextPage.attr('href');
+// 				$.getScript(href);
+// 			}	
+// 		}
+// 	});
+// 	$('.alert').delay(5000).fadeOut(1000);
+// })
